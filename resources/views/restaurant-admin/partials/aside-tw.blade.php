65dc7fb4de26934aa1b8588a4f22a91578bd28a9
Reda Makarem <makarem.reda@gmail.com>
2023-03-27 14:53:35 +0300

    ..

--- resources/views/restaurant-admin/partials/aside-tw.blade.php
+++ resources/views/restaurant-admin/partials/aside-tw.blade.php
@@ -7,19 +7,20 @@
                     <i class="fa fa-desktop"></i>
                 </div>
                 <span class="menu-item-text">Dashboard</span>
-                
+
             </a>
         </div>
+        
         <div class="menu-item">
             <a href="{{ route('restaurant-admin.reports.index') }}" data-menu-path="/index.html" class="menu-item-link">
                 <div class="menu-item-icon">
                     <i class="fa fa-chart-pie"></i>
                 </div>
                 <span class="menu-item-text">Reports</span>
-                
+
             </a>
         </div>
-        
+
         <div class="menu-item">
             <button class="menu-item-link menu-item-toggle">
                 <div class="menu-item-icon">
@@ -32,33 +33,37 @@
             </button>
             <!-- BEGIN Menu Submenu -->
             <div class="menu-submenu">
-                
+
                 <div class="menu-item">
-                    <a href="{{route('restaurant-admin.users.roles',['role' => 4])}}" data-menu-path="/elements/base/badge.html" class="menu-item-link">
+                    <a href="{{ route('restaurant-admin.users.roles', ['role' => 4]) }}"
+                        data-menu-path="/elements/base/badge.html" class="menu-item-link">
                         <i class="menu-item-bullet"></i>
                         <span class="menu-item-text">Rest. Super Admin</span>
                     </a>
                 </div>
                 <div class="menu-item">
-                    <a href="{{route('restaurant-admin.users.roles',['role' => 5])}}" data-menu-path="/elements/base/breadcrumb.html" class="menu-item-link">
+                    <a href="{{ route('restaurant-admin.users.roles', ['role' => 5]) }}"
+                        data-menu-path="/elements/base/breadcrumb.html" class="menu-item-link">
                         <i class="menu-item-bullet"></i>
                         <span class="menu-item-text">Rest. Admin</span>
                     </a>
                 </div>
                 <div class="menu-item">
-                    <a href="{{route('restaurant-admin.users.roles',['role' => 6])}}" data-menu-path="/elements/base/button.html" class="menu-item-link">
+                    <a href="{{ route('restaurant-admin.users.roles', ['role' => 6]) }}"
+                        data-menu-path="/elements/base/button.html" class="menu-item-link">
                         <i class="menu-item-bullet"></i>
                         <span class="menu-item-text">Rest. Host</span>
                     </a>
                 </div>
                 <div class="menu-item">
-                    <a href="{{route('restaurant-admin.users.roles',['role' => 7])}}" data-menu-path="/elements/base/button-group.html" class="menu-item-link">
+                    <a href="{{ route('restaurant-admin.users.roles', ['role' => 7]) }}"
+                        data-menu-path="/elements/base/button-group.html" class="menu-item-link">
                         <i class="menu-item-bullet"></i>
                         <span class="menu-item-text">Rest. Manager</span>
                     </a>
                 </div>
-                
-                
+
+
             </div>
             <!-- END Menu Submenu -->
         </div>
@@ -68,7 +73,7 @@
                     <i class="fa fa-desktop"></i>
                 </div>
                 <span class="menu-item-text">Bookings</span>
-                
+
             </a>
         </div>
         <div class="menu-item">
@@ -84,30 +89,43 @@
             <!-- BEGIN Menu Submenu -->
             <div class="menu-submenu">
                 <div class="menu-item">
-                    <a href="{{route('restaurant-admin.restaurants.index')}}" data-menu-path="/elements/advanced/avatar.html" class="menu-item-link">
+                    <a href="{{ route('restaurant-admin.restaurants.index') }}"
+                        data-menu-path="/elements/advanced/avatar.html" class="menu-item-link">
                         <i class="menu-item-bullet"></i>
                         <span class="menu-item-text">Branches</span>
                     </a>
                 </div>
                 <div class="menu-item">
-                    <a href="{{route('restaurant-admin.schedules.index')}}" data-menu-path="/elements/advanced/block-ui.html" class="menu-item-link">
+                    <a href="{{ route('restaurant-admin.schedules.index') }}"
+                        data-menu-path="/elements/advanced/block-ui.html" class="menu-item-link">
                         <i class="menu-item-bullet"></i>
                         <span class="menu-item-text">Schedules</span>
                     </a>
                 </div>
-                
+
                 <div class="menu-item">
-                    <a href="{{route('restaurant-admin.dishes-new.index')}}" data-menu-path="/elements/advanced/chat.html" class="menu-item-link">
+                    <a href="{{ route('restaurant-admin.dishes-new.index') }}"
+                        data-menu-path="/elements/advanced/chat.html" class="menu-item-link">
                         <i class="menu-item-bullet"></i>
                         <span class="menu-item-text">Dishes</span>
                     </a>
                 </div>
-                
-                
+
+
+
             </div>
             <!-- END Menu Submenu -->
         </div>
-        
+        @if (session('impersonated_by'))
+            <div class="menu-item">
+                <a href="{{ route('users.leave-impersonate') }}" data-menu-path="/index.html" class="menu-item-link">
+                    <div class="menu-item-icon">
+                        <i class="fa fa-sign-out"></i>
+                    </div>
+                    <span class="menu-item-text">End Impersonation</span>
+                </a>
+            </div>
+        @endif
     </div>
     <!-- END Menu -->
 </div>
