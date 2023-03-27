41c8f84b4e28d0c4956fb7dcd484d1c9e12a3891
Reda Makarem <makarem.reda@gmail.com>
2023-03-27 14:31:56 +0300

    ..

--- resources/views/site/mobile/restaurant-menu.blade.php
+++ resources/views/site/mobile/restaurant-menu.blade.php
@@ -6,6 +6,25 @@
                 <h1 class="text-center font-lato  font-medium text-xl text-black uppercase tracking-[1.0rem] rtl:font-ahlan rtl:tracking-normal">{{$restaurant->{'name_' . app()->getLocale()} }}</h1>
                 <h4 class="text-center font-lato  font-medium text-xs text-black uppercase tracking-[0.6rem] rtl:font-ahlan rtl:tracking-normal">{{$restaurant->areaa->governate->{'name_'.app()->getLocale()} }}</h4>
                 <h4 class="text-center font-lato  font-medium text-[0.6rem] text-black uppercase tracking-[0.4rem] rtl:font-ahlan rtl:tracking-normal mt-5">&quot; {{__('nadil.general.slogan')}} &quot;</h4>
+                <div class="text-center my-4">
+                    <div>Accessible: @if ($restaurant->accessible)
+                            <span><i class="fas fa-check text-green-500"></i></span>
+                        @else
+                            <span><i class="fas fa-times text-red-500"></i></span>
+                        @endif
+
+                    </div>
+                    <div>Private Rooms:@if ($restaurant->private_rooms)
+                            <span><i class="fas fa-check text-green-500"></i></span>
+                        @else
+                            <span><i class="fas fa-times text-red-500"></i></span>
+                        @endif
+                    </div>
+                    @if (!empty($restaurant->dress_code))
+                        <div>Dress Code: <span>{{ $restaurant->dress_code }}</span></div>
+                    @endif
+
+                </div>
                 <h4 class="text-center font-lato text-xs text-black uppercase tracking-[0.5rem] rtl:font-ahlan rtl:tracking-normal font-bold mt-5">{{__('nadil.dishes.explore_more')}}</h4>
             </div>
             @livewire('site.mobile.restaurant.menu',[$restaurant])
--- resources/views/site/restaurant-menu.blade.php
+++ resources/views/site/restaurant-menu.blade.php
@@ -1,85 +1,100 @@
 @extends('layouts.site-tw')
 @section('content')
+    <div id="page-wrapper"
+        style="background-image:url('{{ $restaurant->getFirstMediaUrl('restaurant_bgs') }}'); background-size: cover">
+        <div id="page-content" class="flex flex-grow flex-col max-w-12xl mx-auto py-[80px] ">
+            <div class="flex">
+                <div id="restaurant-details" class="flex flex-col items-center w-[375px] rounded-[64px] border-2 bg-white">
+                    <h2
+                        class="ltr:font-lato rtl:font-ahlan text-4xl ltr:tracking-widest rtl:tracking-normal mt-16 uppercase text-center">
+                        {{ $restaurant->{'name_' . app()->getLocale()} }}</h2>
+                    <hr class="h-1 w-48 mt-4" />
+                    <a href="{{ route('site.restaurants.book', $restaurant->id) }}"
+                        class="uppercase mt-12 px-16 py-6 bg-nadilBtn-100 ltr:tracking-[6px] rtl:tracking-normal rounded-[19px]">{{ __('nadil.booking.book_now') }}</a>
+                    <div class="uppercase ltr:font-din rtl:font-ahlan text-xl ltr:tracking-[6px] rtl:tracking-normal mt-3">
+                        {{ $restaurant->areaa->governate->{'name_' . app()->getLocale()} }}</div>
+                    <div class="uppercase  text-center font-din text-md tracking-[3px] rtl:font-ahlan rtl:tracking-normal">
+                        {{ $restaurant->areaa->{'name_' . app()->getLocale()} }}</div>
+                    <div class="uppercase  text-center font-din text-md tracking-[3px] rtl:font-ahlan rtl:tracking-normal">
+                        {{ $restaurant->opening_hours }}</div>
+                    <div class="px-8 flex w-full">
+                        <div id="googleMap" class="flex mt-6 flex-grow rounded-[64px] h-[183px] shadow-md"></div>
+                    </div>
+                    <div>
+                        <div>Accessible: @if ($restaurant->accessible)
+                                <span><i class="fas fa-check text-green-500"></i></span>
+                            @else
+                                <span><i class="fas fa-times text-red-500"></i></span>
+                            @endif
 
-<div id="page-wrapper" style="background-image:url('{{$restaurant->getFirstMediaUrl('restaurant_bgs')}}'); background-size: cover">
-    <div id="page-content" class="flex flex-grow flex-col max-w-12xl mx-auto py-[80px] ">
-        <div class="flex">
-            <div id="restaurant-details"
-                 class="flex flex-col items-center w-[375px] rounded-[64px] border-2 bg-white">
-                <h2 class="ltr:font-lato rtl:font-ahlan text-4xl ltr:tracking-widest rtl:tracking-normal mt-16 uppercase text-center">{{ $restaurant->{'name_'.app()->getLocale()}  }}</h2>
-                <hr class="h-1 w-48 mt-4" />
-                <a href="{{route('site.restaurants.book',$restaurant->id)}}" class="uppercase mt-12 px-16 py-6 bg-nadilBtn-100 ltr:tracking-[6px] rtl:tracking-normal rounded-[19px]">{{__('nadil.booking.book_now')}}</a>
-                <div class="uppercase ltr:font-din rtl:font-ahlan text-xl ltr:tracking-[6px] rtl:tracking-normal mt-3">{{$restaurant->areaa->governate->{'name_'.app()->getLocale()} }}</div>
-                <div class="uppercase  text-center font-din text-md tracking-[3px] rtl:font-ahlan rtl:tracking-normal">{{$restaurant->areaa->{'name_'.app()->getLocale()} }}</div>
-                <div class="uppercase  text-center font-din text-md tracking-[3px] rtl:font-ahlan rtl:tracking-normal">{{$restaurant->opening_hours }}</div>
-                <div class="px-8 flex w-full">
-                    <div id="googleMap" class="flex mt-6 flex-grow rounded-[64px] h-[183px] shadow-md"></div>
-                </div>
-                <div>
-                    <div>Accessible: @if ($restaurant->accessible)
-                        <span><i class="fas fa-check text-green-500"></i></span>
-                        @else
-                    <span><i class="fas fa-times text-red-500"></i></span>
-                    @endif
-                    
-                </div>
-                    <div>Private Rooms:@if ($restaurant->private_rooms)
-                        <span><i class="fas fa-check text-green-500"></i></span>
-                        @else
-                    <span><i class="fas fa-times text-red-500"></i></span>
-                    @endif</div>
-                    @if (!empty($restaurant->dress_code))
-                    <div>Dress Code: <span>{{$restaurant->dress_code}}</span></div>
+                        </div>
+                        <div>Private Rooms:@if ($restaurant->private_rooms)
+                                <span><i class="fas fa-check text-green-500"></i></span>
+                            @else
+                                <span><i class="fas fa-times text-red-500"></i></span>
+                            @endif
+                        </div>
+                        @if (!empty($restaurant->dress_code))
+                            <div>Dress Code: <span>{{ $restaurant->dress_code }}</span></div>
                         @endif
-                    
-                </div>
-                <div class="flex w-full px-4 mt-6 space-x-8 justify-center">
-                        <a href="tel://{{$restaurant->phone}}" class="uppercase font-lato rtl:tracking-normal rtl:font-ahlan px-8 py-4 bg-nadilBtn-100 tracking-[6px] rounded-[19px]">{{__('nadil.general.phone')}}</a>
-                        <a href="mailto:{{$restaurant->email}}" class="uppercase font-lato rtl:tracking-normal rtl:font-ahlan px-8 py-4 bg-nadilBtn-100 tracking-[6px] rounded-[19px]">{{__('nadil.general.email')}}</a>
-                </div>
-                <div class="flex w-full px-4 mt-6 space-x-8 justify-center">
+
+                    </div>
+                    <div class="flex w-full px-4 mt-6 space-x-8 justify-center">
+                        <a href="tel://{{ $restaurant->phone }}"
+                            class="uppercase font-lato rtl:tracking-normal rtl:font-ahlan px-8 py-4 bg-nadilBtn-100 tracking-[6px] rounded-[19px]">{{ __('nadil.general.phone') }}</a>
+                        <a href="mailto:{{ $restaurant->email }}"
+                            class="uppercase font-lato rtl:tracking-normal rtl:font-ahlan px-8 py-4 bg-nadilBtn-100 tracking-[6px] rounded-[19px]">{{ __('nadil.general.email') }}</a>
+                    </div>
+                    <div class="flex w-full px-4 mt-6 space-x-8 justify-center">
                         @if (!empty($restaurant->facebook))
-                            <a href="{{$restaurant->facebook}}" class="uppercase px-4 py-4 bg-nadilBtn-100 tracking-[6px] rounded-[19px]"><i class="fa-brands fa-facebook-f"></i></a>
+                            <a href="{{ $restaurant->facebook }}"
+                                class="uppercase px-4 py-4 bg-nadilBtn-100 tracking-[6px] rounded-[19px]"><i
+                                    class="fa-brands fa-facebook-f"></i></a>
                         @endif
                         @if (!empty($restaurant->instagram))
-                            <a href="{{$restaurant->instagram}}" class="uppercase px-4 py-4 bg-nadilBtn-100 tracking-[6px] rounded-[19px]"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="{{ $restaurant->instagram }}"
+                                class="uppercase px-4 py-4 bg-nadilBtn-100 tracking-[6px] rounded-[19px]"><i
+                                    class="fa-brands fa-instagram"></i></a>
                         @endif
-                        
+
+                    </div>
+
+                    <div class="mt-16 pb-28 uppercase font-lato rtl:font-ahlan rtl:tracking-normal font-italic">
+                        &quot;{{ __('nadil.general.slogan') }} &quot;</div>
                 </div>
-    
-                <div class="mt-16 pb-28 uppercase font-lato rtl:font-ahlan rtl:tracking-normal font-italic">&quot;{{__('nadil.general.slogan')}} &quot;</div>
-            </div>
-    
-            <div class="w-12"></div>
-    
-            <div id="menu"
-                 class="flex flex-1 px-16 py-16 min-h-80 rounded-[64px] border-2 bg-white">
-                @livewire('site.restaurant.menu',[$restaurant])
+
+                <div class="w-12"></div>
+
+                <div id="menu" class="flex flex-1 px-16 py-16 min-h-80 rounded-[64px] border-2 bg-white">
+                    @livewire('site.restaurant.menu', [$restaurant])
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 @endsection
 
 @push('scripts')
     <script>
         jQuery('#inlinePicker').datepicker();
-        document.addEventListener('load', function () {
+        document.addEventListener('load', function() {
             initMap();
 
 
 
 
         })
+
         function initMap() {
-            var coordsStr = "{{$restaurant->coordinates}}";
+            var coordsStr = "{{ $restaurant->coordinates }}";
             const myArray = coordsStr.split(",");
             const lat = myArray[0];
             const lng = myArray[1];
 
             console.log(coordsStr.toString());
-            const myLatlng = { lat: parseFloat(lat), lng: parseFloat(lng) };
+            const myLatlng = {
+                lat: parseFloat(lat),
+                lng: parseFloat(lng)
+            };
             const map = new google.maps.Map(document.getElementById("googleMap"), {
                 zoom: 17,
                 center: myLatlng,
@@ -94,5 +109,6 @@
         }
     </script>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfJsNn93pyzgF-9ICzEI1q-N9UN3c0SxE&callback=initMap"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfJsNn93pyzgF-9ICzEI1q-N9UN3c0SxE&callback=initMap">
+    </script>
 @endpush
