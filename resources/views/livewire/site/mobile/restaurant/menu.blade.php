192ab3e4db52caa3e367e65a7fa0cfebda855ecb
Reda Makarem <makarem.reda@gmail.com>
2023-08-10 00:38:08 +0300

    ..

--- resources/views/livewire/site/mobile/restaurant/menu.blade.php
+++ resources/views/livewire/site/mobile/restaurant/menu.blade.php
@@ -3,39 +3,43 @@
     <div class="px-4">
         <div id="menu-bg" class="mx-2 bg-[#F5F5F5] shadow-md rounded-[16px] py-6 px-5 ">
             @forelse ($categories as $category )
-                @if ($category->dishes->count()>0)
-                <h2 class="text-center uppercase font-bold font-lato rtl:font-ahlan rtl:tracking-normal tracking-[4px] mb-4">
-                    {{ $category->{'name_' . app()->getLocale()} }}</h2>
-                @forelse ($category->dishes as $dish)
-                    @if ($dish->is_featured)
-                        <div class="bg-[#E0E0E0] rounded-xl shadow-md py-5 px-4 mb-4">
-                            <div class="flex flex-col">
-                                <div class="flex">
-                                    <div class="dish-img">
-                                        <img class="rounded-full w-24 h-24"
-                                            src="{{ $dish->getFirstMediaUrl('dish_images') }}"
-                                            alt="{{ $dish->{'name_' . app()->getLocale()} }}">
+                @if ($category->dishes->count() > 0)
+                    <h2
+                        class="text-center uppercase font-bold font-lato rtl:font-ahlan rtl:tracking-normal tracking-[4px] mb-4">
+                        {{ $category->{'name_' . app()->getLocale()} }}</h2>
+                    @forelse ($category->dishes as $dish)
+                        @if ($dish->is_featured)
+                            <div class="bg-[#E0E0E0] rounded-xl shadow-md py-5 px-4 mb-4">
+                                <div class="flex flex-col">
+                                    <div class="flex">
+                                        <div class="dish-img">
+                                            <img class="rounded-full w-24 h-24"
+                                                src="{{ $dish->getFirstMediaUrl('dish_images') }}"
+                                                alt="{{ $dish->{'name_' . app()->getLocale()} }}">
+                                        </div>
+                                        <div class="flex flex-col justify-center items-center w-full">
+                                            <div class="uppercase font-lato font-thin text-sm">Nadil recommends</div>
+                                            <div
+                                                class="uppercase font-lato font-semibold rtl:font-ahlan rtl:tracking-normal">
+                                                {{ $dish->{'name_' . app()->getLocale()} }}</div>
+                                        </div>
                                     </div>
-                                    <div class="flex flex-col justify-center items-center w-full">
-                                        <div class="uppercase font-lato font-thin text-sm">Nadil recommends</div>
-                                        <div class="uppercase font-lato font-semibold rtl:font-ahlan rtl:tracking-normal">
-                                            {{ $dish->{'name_' . app()->getLocale()} }}</div>
-                                    </div>
+
                                 </div>
-                                
                             </div>
-                        </div>
-                    @else
-                        <div class="my-8">
-                            <h3 class="uppercase font-lato text-center text-xs rtl:font-ahlan rtl:tracking-normal">{{ $dish->{'name_' . app()->getLocale()} }}
-                            </h3>
-                            <div class="font-lato rtl:font-ahlan rtl:tracking-normal mx-3 font-thin text-center text-xs">
-                                {{ $dish->{'description_' . app()->getLocale()} }}</div>
-                        </div>
-                    @endif
-                @empty
-                    <div class="text-center">No dishes</div>
-                @endforelse
+                        @else
+                            <div class="my-8">
+                                <h3 class="uppercase font-lato text-center text-xs rtl:font-ahlan rtl:tracking-normal">
+                                    {{ $dish->{'name_' . app()->getLocale()} }}
+                                </h3>
+                                <div
+                                    class="font-lato rtl:font-ahlan rtl:tracking-normal mx-3 font-thin text-center text-xs">
+                                    {{ $dish->{'description_' . app()->getLocale()} }}</div>
+                            </div>
+                        @endif
+                    @empty
+                        <div class="text-center">No dishes</div>
+                    @endforelse
                 @endif
             @empty
                 <div>Something went wrong</div>
@@ -44,9 +48,8 @@
     </div>
 
     <div id="btn-booking-wrapper" class="bottom-0 fixed flex justify-center my-2 w-full z-10 px-4">
-        <a
-        class=" bg-nadilBtn-100 font-lato px-6 py-4 rounded-xl rtl:font-ahlan rtl:tracking-normal shadow-lg text-center uppercase w-full border-2" 
-        href="{{route('site.restaurants.book',$restaurant->id)}}">{{__('nadil.booking.book_now')}}</a>
+        <a class=" bg-nadilBtn-100 font-lato px-6 py-4 rounded-xl rtl:font-ahlan rtl:tracking-normal shadow-lg text-center uppercase w-full border-2"
+            href="{{ route('site.restaurants.book', $restaurant->id) }}">{{ __('nadil.booking.book_now') }}</a>
     </div>
 
     <div class="flex justify-center my-12">
@@ -58,8 +61,10 @@
     <div class="flex flex-col items-center my-4">
         <div class="my-4 font-lato font-thin uppercase tracking-[0.6rem]">Contact Info</div>
         <div class="flex justify-center w-full space-x-4">
-            <a class="bg-nadilBtn-100 rounded-xl shadow-lg px-6 py-2 uppercase font-lato" href="#">{{__('nadil.general.email')}}</a>
-            <a class="bg-nadilBtn-100 rounded-xl shadow-lg px-6 py-2 uppercase font-lato" href="#">{{__('nadil.general.phone')}}</a>
+            <a class="bg-nadilBtn-100 rounded-xl shadow-lg px-6 py-2 uppercase font-lato"
+                href="#">{{ __('nadil.general.email') }}</a>
+            <a class="bg-nadilBtn-100 rounded-xl shadow-lg px-6 py-2 uppercase font-lato"
+                href="#">{{ __('nadil.general.phone') }}</a>
         </div>
         <div class="my-4 font-lato font-thin uppercase tracking-[0.6rem]">Socials</div>
         <div class="flex justify-center w-full space-x-4 mb-24">
@@ -69,7 +74,8 @@
                     <i class="fa-brands fa-facebook-f"></i></a>
             @endif
             @if (!empty($restaurant->instagram))
-                <a href="{{ $restaurant->instagram }}" class="flex justify-center items-center uppercase bg-nadilBtn-100 rounded-xl w-12 h-12 text-center"><i
+                <a href="{{ $restaurant->instagram }}"
+                    class="flex justify-center items-center uppercase bg-nadilBtn-100 rounded-xl w-12 h-12 text-center"><i
                         class="fa-brands fa-instagram"></i></a>
             @endif
         </div>
@@ -79,33 +85,51 @@
 
 @push('scripts')
     <script>
-        function offsetBottom(el, i) { i = i || 0; return $(el)[i].getBoundingClientRect().bottom }
+        function isOnScreen(elem) {
+            // if the element doesn't exist, abort
+            if (elem.length == 0) {
+                return;
+            }
+            var $window = jQuery(window)
+            var viewport_top = $window.scrollTop()
+            var viewport_height = $window.height()
+            var viewport_bottom = viewport_top + viewport_height
+            var $elem = jQuery(elem)
+            var top = $elem.offset().top
+            var height = $elem.height()
+            var bottom = top + height
 
-// Returns right offset value
-function offsetRight(el, i) { i = i || 0; return $(el)[i].getBoundingClientRect().right }
-console.log(jQuery('#menu-bg').offset().top + jQuery('#menu-bg').height());
-        jQuery(document).scroll(function () {
-                var y = jQuery(this).scrollTop();
-                console.log(y);
-                if (y > (jQuery('#menu-bg').offset().top + jQuery('#menu-bg').height() )) {
+            return (top >= viewport_top && top < viewport_bottom) ||
+                (bottom > viewport_top && bottom <= viewport_bottom) ||
+                (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+        }
+
+        jQuery(document).ready(function() {
+            window.addEventListener('scroll', function(e) {
+                if (isOnScreen(jQuery('#googleMap'))) {
+                    /* Pass element id/class you want to check */
                     jQuery('#btn-booking-wrapper').removeClass('fixed');
                 } else {
                     jQuery('#btn-booking-wrapper').addClass('fixed');
                 }
             });
-        // jQuery('#inlinePicker').datepicker();
-        document.addEventListener('load', function () {
+        });
+        document.addEventListener('load', function() {
             initMap();
-            
+
         })
+
         function initMap() {
-            var coordsStr = "{{$restaurant->coordinates}}";
+            var coordsStr = "{{ $restaurant->coordinates }}";
             const myArray = coordsStr.split(",");
             const lat = myArray[0];
             const lng = myArray[1];
 
             console.log(coordsStr.toString());
-            const myLatlng = { lat: parseFloat(lat), lng: parseFloat(lng) };
+            const myLatlng = {
+                lat: parseFloat(lat),
+                lng: parseFloat(lng)
+            };
             const map = new google.maps.Map(document.getElementById("googleMap"), {
                 zoom: 17,
                 center: myLatlng,
@@ -120,6 +144,6 @@ console.log(jQuery('#menu-bg').offset().top + jQuery('#menu-bg').height());
         }
     </script>
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfJsNn93pyzgF-9ICzEI1q-N9UN3c0SxE&callback=initMap"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfJsNn93pyzgF-9ICzEI1q-N9UN3c0SxE&callback=initMap">
+    </script>
 @endpush
-
